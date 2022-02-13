fec811a51cba05e00185faf2def65d1c7343498f
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-02-13 13:24:16 -0500

    sort.h update: prototypes with new argument

--- sort.h
+++ sort.h
@@ -23,7 +23,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-size_t partition(int *array, int start, int end);
-void handle(int *array, int start, int end);
+size_t partition(int *array, int start, int end, size_t size);
+void handle(int *array, int start, int end, size_t size);
 
 #endif
